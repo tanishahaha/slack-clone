a2237a0a4dbaf0ad50e5ab55f98e6866323c66e4
tanishahaha <amintanisha17@gmail.com>
2024-11-14 19:00:27 +0530

    hoja bhai

--- src/components/main-content.tsx
+++ src/components/main-content.tsx
@@ -18,7 +18,7 @@ const MainContent: FC<{ children: ReactNode }> = ({ children }) => {
 
 
     return (
-        <div className={cn('md:px-2 md:pb-2 md:h-screen md:pt-14', backgroundColor)}>
+        <div className={cn('md:px-2 md:pb-2 md:h-screen md:pt-14 text-black', backgroundColor)}>
             <main className={cn('md:ml-[280px] lg:ml-[420px] md:h-full overflow-y-hidden', theme === 'dark' ? 'bg-[#232529]' : 'bg-white')}>
                 {children}
             </main>
